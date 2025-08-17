52da1b36e362d269ac32cfbb309d4ad3f4be82ce
Sanket Pandit <sanketp@amd.com>
2025-08-17 20:50:45 +0000

    working dist_gemm and test
    Signed-off-by: Sanket Pandit <sanketp@amd.com>

--- tests/kernel/wave/dist_gemm_test.py
+++ tests/kernel/wave/dist_gemm_test.py
@@ -40,13 +40,11 @@ from torch.testing import assert_close
 
 
 @require_e2e
-@pytest.mark.skip(reason="Test not ready - will be enabled in follow-up PR")
 @pytest.mark.parametrize("shape", [(128, 128, 64)])
 @pytest.mark.parametrize(
     "mfma_variant",
     [
         MMAType.F32_16x16x16_F16,
-        MMAType.F32_32x32x8_F16,
     ],
 )
 @pytest.mark.parametrize("datatype", [torch.float16])
@@ -61,10 +59,11 @@ def testPureGemm(
 
     options = WaveCompileOptions(
         subs=hyperparams,
+        iree_launch_async=False,
     )
     options = set_default_run_config(options)
     gemm = wave_compile(options, gemm)
-
+    print(gemm.asm)
     a = device_randn(shape[0], shape[2], dtype=datatype)
     b = device_randn(shape[1], shape[2], dtype=datatype)
     c = device_zeros(shape[0], shape[1], dtype=torch.float32)
