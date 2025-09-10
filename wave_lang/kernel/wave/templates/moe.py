e25f528c65ada8157aeb6d20f45704ef3b13796c
Sanket Pandit <sanketp@amd.com>
2025-09-10 23:02:12 +0000

    create inclusive and exclusive cumsum buffers

--- wave_lang/kernel/wave/templates/moe.py
+++ wave_lang/kernel/wave/templates/moe.py
@@ -12,16 +12,6 @@ from wave_lang.kernel._support.dtype import DataType
 import sympy
 import torch
 
-# Copyright 2025 The IREE Authors
-#
-# Licensed under the Apache License v2.0 with LLVM Exceptions.
-# See https://llvm.org/LICENSE.txt for license information.
-# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-from wave_lang.kernel.lang.global_symbols import *
-from wave_lang.kernel.wave.constraints import MMAType
-from wave_lang.kernel._support.dtype import DataType
-
 from wave_lang.kernel.wave.utils.general_utils import (
     get_default_scheduling_params,
     torch_dtype_to_wave,
@@ -48,7 +38,10 @@ def get_moe_align_block_size_kernel(
     NUMEL = tkl.sym.NUMEL
     TOPK = tkl.sym.TOPK
     BLOCK_SIZE = tkl.sym.BLOCK_SIZE
+    OFFSET = tkl.sym.OFFSET
+    ne = tkl.sym.ne
 
+    bindings = {ne: NUM_EXPERTS}
     # Workgroup tile sizes
     BLOCK_TOKENS = tkl.sym.BLOCK_TOKENS
     BLOCK_EXPERTS = tkl.sym.BLOCK_EXPERTS
@@ -67,6 +60,7 @@ def get_moe_align_block_size_kernel(
     constraints += [tkw.WaveConstraint(NUM_EXPERTS, NUM_EXPERTS)]
     # constraints += [tkw.TilingConstraint(NUMEL, NUMEL)]
 
+    constraints += [tkw.IteratorBindings(bindings)]
     constraints += [
         tkw.HardwareConstraint(
             threads_per_wave=64,
@@ -92,11 +86,31 @@ def get_moe_align_block_size_kernel(
         dynamic_val_mappings={NUM_EXPERTS: i},
     )
 
+    shifted_read_map = tkw.IndexMapping(
+        num_iterators=1,
+        inputs={NUM_EXPERTS: d0},
+        outputs={NUM_EXPERTS: i},
+        dynamic_val_mappings={NUM_EXPERTS: i},
+    )
+
+    shifted_write_map = tkw.IndexMapping(
+        num_iterators=1,
+        inputs={NUM_EXPERTS: i},
+        outputs={NUM_EXPERTS: d0 + 1},
+        dynamic_val_mappings={NUM_EXPERTS: i},
+    )
+
     simple_map = tkw.IndexMapping(
         num_iterators=1,
         inputs={NUM_EXPERTS: i},
         outputs={NUM_EXPERTS: i},
     )
+
+    printer_args = None
+
+    def printer(*args):
+        nonlocal printer_args
+        printer_args = args
 
     @tkw.wave(constraints)
     def moe_align_block_size(
@@ -110,16 +124,30 @@ def get_moe_align_block_size_kernel(
         cumsum_buffer: tkl.Memory[
             NUM_EXPERTS, tkl.global_symbols.GLOBAL_ADDRESS_SPACE, dtype
         ],
+        num_blocks_buffer: tkl.Memory[
+            NUM_EXPERTS, tkl.global_symbols.GLOBAL_ADDRESS_SPACE, dtype
+        ],
     ):
 
         tid = tkw.scalar(THREAD_0, tkl.i32)
+        num_experts = tkw.scalar(NUM_EXPERTS - 1, tkl.i32)
         zero_counts = tkl.Register[NUM_EXPERTS, dtype](0)
         one_reg = tkw.Register[NUM_EXPERTS, dtype](1)
+        shifted_cumsum = tkw.Register[NUM_EXPERTS, dtype](0)
+        tkw.set_symbol(OFFSET, tkw.scalar(1, tkl.i32))
 
         shmem = tkw.allocate(
             shape=(NUM_EXPERTS,),
             distributed_shape=(NUM_EXPERTS,),
             dtype=dtype,
+        )
+        cumsum_exclusive = tkw.allocate(
+            shape=(NUM_EXPERTS,),
+            distributed_shape=(NUM_EXPERTS,),
+            dtype=dtype,
+        )
+        s_total_tokens_post_pad = tkw.allocate(
+            (1,), distributed_shape=(1,), dtype=dtype
         )
         tkw.write(zero_counts, shmem)
 
@@ -138,6 +166,8 @@ def get_moe_align_block_size_kernel(
             mapping_dynamic_vals=(tid,),
             elements_per_thread=1,
         )
+
+        # write the histogram counts to global memory
         tkw.write(
             counts,
             expert_counts,
@@ -146,7 +176,7 @@ def get_moe_align_block_size_kernel(
             elements_per_thread=1,
         )
 
-        # # Implement the padding logic
+        # Implement the padding logic
         block_size_reg = tkl.Register[NUM_EXPERTS, dtype](BLOCK_SIZE)
 
         # (count + block_size - 1) // block_size * block_size
@@ -169,9 +199,37 @@ def get_moe_align_block_size_kernel(
         )
 
         prefix_sums = tkw.cumsum(padded_counts_reg, dim=NUM_EXPERTS)
+
+        # write the inclusive scan results to global memory
         tkw.write(
             prefix_sums,
             cumsum_buffer,
+            mapping=expert_write_map,
+            mapping_dynamic_vals=(tid,),
+            elements_per_thread=1,
+        )
+
+        # write the exclusive scan results to the shared memory
+        tkw.write(
+            prefix_sums,
+            cumsum_buffer,
+            mapping=shifted_write_map,
+            mapping_dynamic_vals=(tid,),
+            elements_per_thread=1,
+        )
+
+        # read the last element from the cumsum buffer to get total tokens after padding
+        total_tokens_post_pad = tkw.read(
+            cumsum_buffer,
+            mapping=expert_read_map,
+            mapping_dynamic_vals=(num_experts,),
+            elements_per_thread=1,
+        )
+
+        num_blocks = total_tokens_post_pad / block_size_reg
+        tkw.write(
+            num_blocks,
+            num_blocks_buffer,
             mapping=expert_write_map,
             mapping_dynamic_vals=(tid,),
             elements_per_thread=1,
