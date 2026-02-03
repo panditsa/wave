ed5b44e99920885578a34a0f2cd974a0f1b768c0
Ivan Butygin <ivan.butygin@gmail.com>
2026-02-03 16:17:44 +0100

    pass stub
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

---
+++ water/lib/Transforms/FuseScaleLoads.cpp
@@ -0,0 +1,28 @@
+// Copyright 2025 The Wave Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "water/Transforms/Passes.h"
+
+#include "mlir/Dialect/AMDGPU/IR/AMDGPUDialect.h"
+#include "mlir/Dialect/Vector/IR/VectorOps.h"
+#include "mlir/Pass/Pass.h"
+
+using namespace mlir;
+
+namespace mlir::water {
+#define GEN_PASS_DEF_WATERFUSESCALELOADSPASS
+#include "water/Transforms/Passes.h.inc"
+} // namespace mlir::water
+
+namespace {
+class FuseScaleLoadsPass
+    : public water::impl::WaterFuseScaleLoadsPassBase<FuseScaleLoadsPass> {
+public:
+  void runOnOperation() override {
+    // TODO: Implement scale load fusion.
+  }
+};
+} // namespace
