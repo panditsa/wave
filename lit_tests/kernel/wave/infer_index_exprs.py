ca6ab0356237c568686448902343e8a2d8659e99
Oleksandr "Alex" Zinenko <git@ozinenko.com>
2026-02-06 15:05:45 +0100

    use gemm template in the water analysis test (#833)
    We previously couldn't use it due to missing support for wave
    constraints and it using (unsafe) unceiled division in them. Both are
    now available with safe workarounds.
    
    ---------
    
    Signed-off-by: Alex Zinenko <git@ozinenko.com>

--- lit_tests/kernel/wave/infer_index_exprs.py
+++ lit_tests/kernel/wave/infer_index_exprs.py
@@ -9,116 +9,41 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 import wave_lang.kernel.lang as tkl
-import wave_lang.kernel.wave as tkw
-from wave_lang.kernel.wave.wave import LaunchableWave
 from wave_lang.kernel.wave.compile import WaveCompileOptions, wave_compile
 
+from wave_lang.kernel.wave.templates.gemm import get_gemm_kernel
 from wave_lang.kernel.lang.global_symbols import *
 from wave_lang.kernel.wave.constraints import MMAType
-from wave_lang.kernel.wave.utils.general_utils import torch_dtype_to_wave
-
-import torch
-
-
-# TODO: use the generic template, currently blocked by water not handling wave constraints.
-def _get_gemm_kernel(
-    shape: tuple[int, int, int],
-    mfma_variant: MMAType,
-    use_shmem: bool = False,
-    dtype: torch.dtype = torch.float16,
-    block_shape: tuple[int, int, int] | None = None,
-    waves_per_block: tuple[int, int] | None = None,
-) -> tuple[LaunchableWave, dict[tkl.IndexSymbol, tkl.IndexExpr]]:
-    if not block_shape:
-        # BLOCK_M, BLOCK_N, BLOCK_K
-        block_shape = (64, 64, 32)
-
-    if not waves_per_block:
-        # WAVE_M, WAVE_N
-        waves_per_block = (2, 2)
-
-    assert len(block_shape) == 3, "block_shape needs to be rank 3 for M, N, K."
-    assert len(waves_per_block) == 2, "waves_per_block needs to be rank 2 for M, N."
-
-    # Input sizes
-    M = tkl.sym.M
-    N = tkl.sym.N
-    K = tkl.sym.K
-    # Workgroup tile sizes
-    BLOCK_M = tkl.sym.BLOCK_M
-    BLOCK_N = tkl.sym.BLOCK_N
-    BLOCK_K = tkl.sym.BLOCK_K
-    # Address space (for GPU, shared(1) or global(0))
-    ADDRESS_SPACE = tkl.sym.GLOBAL_ADDRESS_SPACE
-    dtype = torch_dtype_to_wave(dtype)
-    # Expose user-constraints
-    constraints: list[tkw.Constraint] = [tkw.WorkgroupConstraint(M, BLOCK_M, 0)]
-    constraints += [tkw.WorkgroupConstraint(N, BLOCK_N, 1)]
-    constraints += [tkw.TilingConstraint(K, BLOCK_K)]
-
-    # TODO: dialect expects waves_per_block to be rank 3, so we append a 1 to the end.
-    constraints += [
-        tkw.HardwareConstraint(
-            threads_per_wave=64,
-            mma_type=mfma_variant,
-            waves_per_block=waves_per_block + (1,),
-        )
-    ]
-
-    # Wave-level micro-kernel.
-    # Since warps are not directly addressable, there is no
-    # explicit notion of a warp id (like a workgroup or thread id).
-    # This kernel uses the input sizes M, N, K throughout, as the tiling
-    # and data movement strategy is determined during the compilation process.
-    # These can be influenced by introducing constraints.
-    @tkw.wave(constraints)
-    def gemm(
-        a: tkl.Memory[M, K, ADDRESS_SPACE, dtype],
-        b: tkl.Memory[N, K, ADDRESS_SPACE, dtype],
-        c: tkl.Memory[M, N, GLOBAL_ADDRESS_SPACE, tkl.f32],
-    ):
-        c_reg = tkl.Register[M, N, tkl.f32](0.0)
-
-        # This microkernel encodes the fact that if the iterate
-        # dimension were tiled, then we would need to materialize a loop.
-        @tkw.iterate(K, init_args=[c_reg])
-        def repeat(acc: tkl.Register[M, N, tkl.f32]) -> tkl.Register[M, N, tkl.f32]:
-            # a_reg: tkw.Register[M, K, dtype]
-            a_reg = tkw.read(a)
-            # b_reg: tkw.Register[N, K, dtype]
-            b_reg = tkw.read(b)
-            # acc: tkw.Register[M, N, tkl.f32]
-            acc = tkw.mma(a_reg, b_reg, acc)
-            return acc
-
-        # repeat represents the results of the loop
-        tkw.write(repeat, c)
-
-    hyperparams = {
-        ADDRESS_SPACE: SHARED_ADDRESS_SPACE if use_shmem else GLOBAL_ADDRESS_SPACE,
-        BLOCK_M: block_shape[0],
-        BLOCK_N: block_shape[1],
-        BLOCK_K: block_shape[2],
-        M: shape[0],
-        N: shape[1],
-        K: shape[2],
-    }
-    return gemm, hyperparams
 
 
 def testGemm():
+    relevant_hyperparams = [
+        tkl.sym.M,
+        tkl.sym.N,
+        tkl.sym.K,
+        tkl.sym.BLOCK_M,
+        tkl.sym.BLOCK_N,
+        tkl.sym.BLOCK_K,
+        tkl.sym.ADDRESS_SPACE,
+    ]
+
     for use_shmem in [True, False]:
         for mfma_variant, target in [
             (MMAType.F32_32x32x16_F16, "gfx950"),
             (MMAType.F32_16x16x16_F16, "gfx942"),
         ]:
             print(f"Testing {mfma_variant} on {target} with LDS={use_shmem}")
-            gemm, hyperparams = _get_gemm_kernel(
-                shape=(1024, 1024, 1024),
-                # shape = (32,	160,	1867),
-                mfma_variant=mfma_variant,
-                use_shmem=use_shmem,
+            gemm, hyperparams, _ = get_gemm_kernel(
+                shape=(1024, 1024, 1024), dynamic_dims=False, mfma_variant=mfma_variant
             )
+
+            # Override usage of shared memory if not requested as the template always uses it.
+            if not use_shmem:
+                hyperparams[tkl.sym.ADDRESS_SPACE] = GLOBAL_ADDRESS_SPACE
+            # Avoid unused hyperparameter warnings
+            hyperparams = {
+                s: v for s, v in hyperparams.items() if s in relevant_hyperparams
+            }
             options = WaveCompileOptions(
                 subs=hyperparams,
                 run_bench=False,
