4e1be05707c374c09e34c2001bc4d510f46813e1
Sanket Pandit <sanket.pandit@amd.com>
2026-02-20 12:19:39 -0600

    cleanup of loop address promotion pass
    Signed-off-by: Sanket Pandit <sanket.pandit@amd.com>

--- wave_lang/kernel/wave/asm/wave_asm/include/waveasm/Transforms/Passes.h
+++ wave_lang/kernel/wave/asm/wave_asm/include/waveasm/Transforms/Passes.h
@@ -54,6 +54,9 @@ std::unique_ptr<mlir::Pass> createWAVEASMScopedCSEPass();
 /// Create the peephole optimization pass
 std::unique_ptr<mlir::Pass> createWAVEASMPeepholePass();
 
+/// Create loop/region structural transformation passes
+std::unique_ptr<mlir::Pass> createWAVEASMLoopAddressPromotionPass();
+
 /// Create the memory offset optimization pass
 std::unique_ptr<mlir::Pass> createWAVEASMMemoryOffsetOptPass();
 
